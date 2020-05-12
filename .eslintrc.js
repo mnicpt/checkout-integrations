316299466d66402058c541e4fc167e4a0b533365
Steven Mask <stevenmask@macbook-pro.lan>
2020-05-12 15:27:13 -0400

    blog

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off"
+  },
+};
